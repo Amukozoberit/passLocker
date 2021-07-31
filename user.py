0a33739aca5b07520080c0a8755cd13df637ad7f
Amukozoberit <mwasheb@gmail.com>
2021-07-31 11:34:38 +0300

    added content to user class

---
+++ user.py
@@ -0,0 +1,11 @@
+import email
+
+
+class User:
+    '''user Class helps define properties for our objects'''
+    def __init__(self,first_Name,last_Name,e_mail,user_Name):
+        self.first_Name=first_Name
+        self.last_Name=last_Name
+        self.e_mail=e_mail
+        self.user_Name=user_Name
+        pass
