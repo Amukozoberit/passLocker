d228ce8d4a29324daf74e81c5a6dbed5df0282c7
Amukozoberit <mwasheb@gmail.com>
2021-07-31 11:45:15 +0300

    added save functionality

--- user.py
+++ user.py
@@ -2,10 +2,15 @@ import email
 
 
 class User:
-    '''user Class helps define properties for our objects'''
+    '''user Class helps define properties for our class everytime
+    we run our class an instant is created with this properties'''
     def __init__(self,first_Name,last_Name,e_mail,user_Name):
         self.first_Name=first_Name
         self.last_Name=last_Name
         self.e_mail=e_mail
         self.user_Name=user_Name
         
+
+
+    def save_contact(self):
+        self.e_mail
