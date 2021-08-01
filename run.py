0ec4e29ef00b5f58b9e5455dc31b92d538c54a04
Amukozoberit <mwasheb@gmail.com>
2021-08-01 13:00:37 +0300

    added docstring to run.py

--- run.py
+++ run.py
@@ -3,13 +3,16 @@ from user import User
 
 
 def createAccount(fname,lname,mail,uName,pass_word):
+    '''Creates user account'''
     new_user=User(first_Name=fname,last_Name=lname,user_Name=uName,e_mail=mail,pass_word=pass_word)
     return new_user
 
 def save_Users(user):
+    '''saves the user details'''
     user.save_User()
 
 def login(pass_word):
+    '''logs in the user by connecting find user'''
     new_user=User.find_User(pass_word)
     return new_user
 
@@ -17,28 +20,35 @@ def login(pass_word):
 
 # Credential functions
 def create_Credentials(name,password):
+    '''Fucntion that creates credentials of the user '''
     new_credentials=Credentials(name=name,password=password)
     return new_credentials
 
 
 def save_Cred(cred):
+    '''saves user credentials using save credetials function in cred page'''
     cred.save_Credentials()
 
 def gen_Pass():
-   return  Credentials.genPass()
+    '''generates password for user'''
+    return  Credentials.genPass()
 
 def display_Cred():
+    '''function that displays user credentials by calling display credentials'''
     return Credentials.display_Credentials()
 
 
 def find_Cred(name):
+    '''function to find the user credentials by allowing user to search'''
     return Credentials.find_cred(name)
 
 
 def check_existing(val):
+    '''function to check if the crediats exist'''
     return Credentials.check_cred_Exists(val)
 
 def del_Cred(del_cred):
+    ''''function to delete the credentials'''
     del_cred.delete_Account()
 
 def main():
@@ -50,6 +60,8 @@ def main():
         print("Do you have an account? Use LI to let me know that you want to login")
         print('\n')
         print("Dont have an account? Use CA to tell me to create an accout for you")
+        print('\n')
+        print("Dont have an account? Use VC to tell me to show you your  accout ")
         print('\n')
         print("Dont you want to Exit? Use EX to tell me to allow you leave") 
 
